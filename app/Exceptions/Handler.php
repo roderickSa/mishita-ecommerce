deecfd46968bcfe62d95939fd682fe6d684ae7e8
roderickSanchez <roderick.sanchez@justo.mx>
2023-12-08 10:52:52 -0500

    CUSTOM VALIDATION FOR RESOURCE NOT FOUND EXCEPTION

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -25,6 +27,12 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            if ($request->is('api/*')) {
+                return response('resource not found', Response::HTTP_NOT_FOUND);
+            }
         });
     }
 }
